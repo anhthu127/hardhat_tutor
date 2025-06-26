91b7e8072fea2867eafc87e60e9921e4ccd51805
thudta127 <thudta127@gmail.com>
2025-06-26 21:56:38 +0700

    first message

---
+++ workshop_01/hardhat.config.js
@@ -0,0 +1,7 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+};
